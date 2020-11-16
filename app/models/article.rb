fbd8a0645984a360a013b38f74122c64acc20d6b
Selvaraj <code@getbraintree.com>
2020-11-16 12:10:32 +0530

    DRY principles with all CRUD operations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: {minimum:5, maximum: 30}
-  validates :description, presence: true, length: {minimum:50, maximum: 3000}
+  validates :title, presence: true, length: { minimum: 5, maximum: 30 }
+  validates :description, presence: true, length: { minimum: 20, maximum: 3000 }
 end
